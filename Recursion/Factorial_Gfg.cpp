9726fca495996cb465be19240c76b9d25a5c0c10
Raghav <guptaraghav2608.com>
2026-03-01 13:28:33 +0530

    Solved: Factorial of a number using recursion in O(N)) time complexity

---
+++ Recursion/Factorial_Gfg.cpp
@@ -0,0 +1,30 @@
+
+
+/*
+Problem: Factorial
+Platform: GeeksforGeeks
+Topic: Recursion
+
+Approach:
+1. Base Case: If n is less than or equal to 0, return 1 (since the factorial of 0 is defined as 1).
+2. Recursive Case: Return n multiplied by the result of the function called with n-1, which will calculate the factorial of n by multiplying n with the factorial of (n-1).
+
+Time Complexity: O(n) since we are making n recursive calls.
+Space Complexity: O(n) due to the recursive call stack, which can go up to n levels deep.
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    long long factorial(int n) {
+        // code here
+        if(n==0 || n==1){
+            return 1;
+        }
+        else{
+            return 1LL * n * factorial(n-1); // Use 1LL to ensure the multiplication is done in long long to prevent overflow for larger values of n.
+        }
+    }
+};

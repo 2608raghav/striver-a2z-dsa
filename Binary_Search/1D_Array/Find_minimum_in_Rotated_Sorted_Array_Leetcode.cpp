d67e9038cc3a387e73a1844ab4b173495586cd18
Raghav <guptaraghav2608.com>
2026-03-30 18:27:24 +0530

    Solved: Find the minimum element in the array in the O(logN) time complexity

---
+++ Binary_Search/1D_Array/Find_minimum_in_Rotated_Sorted_Array_Leetcode.cpp
@@ -0,0 +1,37 @@
+/*
+Problem: Find Minimum in Rotated Sorted Array
+Platform: Leetcode
+Topic: Binary Search
+
+Approach:
+1. Initialize two pointers, one at the beginning and one at the end of the array.
+2. Calculate the middle index.
+3. If the element at the middle index is less than the element at the end index, it means the minimum element is in the left half of the array. Therefore, update the end pointer to mid.
+4. If the element at the middle index is greater than the element at the end index,
+it means the minimum element is in the right half of the array. Therefore, update the start pointer to mid + 1.
+5. Repeat steps 2-4 until the start pointer is equal to the end pointer. The element at the start (or end) pointer will be the minimum element in the array.
+
+
+Time Complexity: O(log n) in the worst and average cases, as we are dividing the search space in half at each step. In the best case (when the array is already sorted), the time complexity is O(1) as we can directly return the first element.
+Space Complexity: O(1) as we are using only a constant amount of extra space.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int start =0;
+        int end = nums.size()-1;
+        while(start<end){
+            int mid = (start+end)/2;
+            if(nums[mid]<nums[end]){
+                end=mid;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return nums[start];
+    }
+};

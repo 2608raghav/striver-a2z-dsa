345df80ab067ed35721e52fa40a45769d202efc5
Raghav <guptaraghav2608.com>
2026-04-07 20:50:04 +0530

    Solved: Median of 2 sorted array in the O(log(min(n, m)) time complexity

---
+++ Binary_Search/1D_Array/Median_of_2_sorted_arrays_Leetcode.cpp
@@ -0,0 +1,55 @@
+/*
+Problem: Median of 2 sorted arrays
+Platform: Leetcode
+Topic: Binary Search
+
+Approach:
+1. We can use binary search to find the median of the two sorted arrays.
+2. We will use two pointers, one for each array, to keep track of the current index in each array.
+3. We will calculate the median by comparing the elements at the current indices of both arrays and moving the pointers accordingly.
+4. We will continue this process until we have found the median.
+
+Time Complexity: O(log(min(n, m))) where n and m are the sizes of the two arrays. We are performing a binary search on the smaller array.
+Space Complexity: O(1) as we are using only a constant amount of extra space.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        if(n1>n2){
+            return findMedianSortedArrays(nums2,nums1);
+        }
+        int low =0;
+        int high = n1;
+        while(low<=high){
+            int cut1 = (low+high)/2;
+            int cut2 = (n1+n2+1)/2 - cut1;
+            
+            int left1 = cut1==0?INT_MIN:nums1[cut1-1];
+            int left2 = cut2==0?INT_MIN:nums2[cut2-1];
+            
+            int right1 = cut1==n1?INT_MAX:nums1[cut1];
+            int right2 = cut2==n2?INT_MAX:nums2[cut2];
+            
+            if(left1<=right2 && left2<=right1){
+                if((n1+n2)%2==0){
+                    return (max(left1,left2)+min(right1,right2))/2.0;
+                }
+                else{
+                    return max(left1,left2);
+                }
+            }
+            else if(left1>right2){
+                high = cut1-1;
+            }
+            else{
+                low = cut1+1;
+            }
+        }
+        return 0.0;
+    }
+};

0cbfbcd2c8476cda5997387a1a21466462be16ba
Raghav <guptaraghav2608.com>
2026-05-07 21:04:26 +0530

    Solved: Pattern2 in the O(N^2) time complexity

---
+++ 01_Basics/Patterns/Pattern2_TUF.cpp
@@ -0,0 +1,29 @@
+/*
+Problem: Print the following pattern for a given N:
+Platform: TUF
+Topic: Basic Math
+
+Approach:
+1. We will use two nested loops to print the pattern.
+2. The outer loop will run from 0 to N-1, which will represent the number of rows.
+3. The inner loop will run from 0 to i, which will represent the number of columns in each row (increasing by one each row).
+4. In each iteration of the inner loop, we will print an asterisk (*).
+5. After the inner loop completes for each row, we will print a newline character to move to the next line.
+
+Time Complexity: O(N^2) because we have two nested loops, where the inner loop runs i times for each iteration of the outer loop, leading to a total of 1 + 2 + ... + N = O(N^2) iterations.
+Space Complexity: O(1) since we are only using a constant amount of extra space for variables.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void pattern2(int n) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+    }
+};

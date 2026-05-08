a008cd1cc297e30f18a545d0e54d86184c4e43a5
Raghav <guptaraghav2608.com>
2026-05-08 23:46:01 +0530

    Solved: Pattern 4 is solved in the O(N^2) time complexity

---
+++ 01_Basics/Patterns/Pattern4_TUF.cpp
@@ -0,0 +1,30 @@
+/*
+Problem: Print the following pattern for a given N:
+Platform: TUF
+Topic: Basic Math
+
+Approach:
+1. We will use two nested loops to print the pattern.
+2. The outer loop will run from 1 to N, which will represent the number of rows.
+3. The inner loop will run from 1 to i, which will represent the number of columns in each row (increasing by one each row).
+4. In each iteration of the inner loop, we will print the value of j, which represents the column number.
+5. After the inner loop completes for each row, we will print a newline character to move to the next line.
+
+Time Complexity: O(N^2) because we have two nested loops, where the inner loop runs i times for each iteration of the outer loop, leading to a total of 1 + 2 + ... + N = O(N^2) iterations.
+Space Complexity: O(1) since we are only using a constant amount of extra space for variables.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void pattern4(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                cout<<i;
+            }
+            cout<<endl;
+        }
+    }
+};

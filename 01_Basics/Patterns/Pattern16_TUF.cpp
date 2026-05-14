1b0f73056aed729b99b536736dbfca1ddbb4eb37
Raghav <guptaraghav2608.com>
2026-05-14 23:15:05 +0530

    Solved : Printed the given pattern in the O(N^2) time complexity

---
+++ 01_Basics/Patterns/Pattern16_TUF.cpp
@@ -0,0 +1,30 @@
+/*
+Problem: Print the following pattern for a given N:
+Platform: TUF
+Topic: Basic Math
+
+Approach:
+1. We will use two nested loops to generate the pattern. The outer loop will iterate from 0 to N-1 for the upper half of the pattern and from N-1 to 0 for the lower half.
+2. The inner loop will iterate from 0 to N-1 and will print either a space or a number based on the condition j <= i for the first half and (k + i) >= n - 1 for the second half.
+3. We will maintain a count variable to keep track of the numbers being printed in each row and use that count to print the additional numbers needed to complete the pattern.
+
+Time Complexity: O(N^2) - We have two nested loops, each running N times.
+Space Complexity: O(1) - We are using a constant amount of space to store the count variable and the loop variables.
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void pattern16(int n) {
+        int count = 65;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                cout<<char(count);
+            }
+            cout<<endl;
+            count++;
+        }
+    }
+};
